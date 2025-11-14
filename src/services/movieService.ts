ca641f820597f753f5d7729447689435c12caa57
Vitalii_gh <a.g.vitalii2022@gmail.com>
2025-11-15 00:01:23 +0200

    new

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,40 +1,33 @@
 import axios from "axios";
 import type { Movie } from "../types/movie";
 
-const BASE_URL = "https://api.themoviedb.org/3";
+const API_URL = "https://api.themoviedb.org/3/search/movie";
+const API_KEY = import.meta.env.VITE_TMDB_TOKEN;
 
-interface MoviesResponse {
+export interface MoviesResponse {
   page: number;
   results: Movie[];
   total_pages: number;
   total_results: number;
 }
 
-export async function fetchMovies(
-  query: string,
-  page = 1
-): Promise<MoviesResponse> {
-  const url = `${BASE_URL}/search/movie`;
-
-  const config = {
+export async function fetchMovies(query: string): Promise<MoviesResponse> {
+  const response = await axios.get<MoviesResponse>(API_URL, {
     params: {
       query,
       include_adult: false,
       language: "en-US",
-      page,
+      page: 1,
     },
     headers: {
       accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZDg4MmRlZTQ3MzE5NmE0MzA5NWM5NGYwNmIwYzE3MiIsIm5iZiI6MTc2Mjg1Mzc0NC45OTUwMDAxLCJzdWIiOiI2OTEzMDM3MGViNjNmY2JiY2ZkMjYxNDIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.QN8Mk_pcl8k91glBEm8IelQBgMv7ruWcr1h74pyZ53E",
+      Authorization: `Bearer ${API_KEY}`,
     },
-  };
+  });
 
-  const response = await axios.get<MoviesResponse>(url, config);
-
-  response.data.results = response.data.results.filter(
-    (movie) => movie.poster_path && movie.backdrop_path
-  );
+  // response.data.results = response.data.results.filter(
+  //   (movie) => movie.poster_path && movie.backdrop_path
+  // );
 
   return response.data;
 }
